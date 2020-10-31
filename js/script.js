2390d86e70248a84bbed982d0aa5263f82716ace
DERUINO <main@deruwork.ru>
2020-10-31 18:16:05 +0300

    snapshot-1.0

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	$('.close_modal').click(function () {
+		$('.modalblock').fadeOut(300);
+	});
+
+	$('.add_button').click(function () {
+		$('.nameblock').append('<div class="name"><input class="new_input" type="text" placeholder="Имя поставщика"></div>');
+	});
+
+	$('.open_table').click(function () {
+		$('.add_table').fadeIn();
+	})
+
+	$('.open_name').click(function () {
+		$('.add_name').fadeIn();
+	})
+});
